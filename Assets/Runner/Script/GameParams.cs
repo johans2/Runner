b1ad117e0e8b85e8f3279d01259ed7c394e11e90
Johan <johansvensson@outlook.com>
2018-04-22 21:03:06 +0200

    Some basic rolling. But really need to remove the physics on the runner.

--- Assets/Runner/Script/GameParams.cs
+++ Assets/Runner/Script/GameParams.cs
@@ -7,5 +7,9 @@ public class GameParams : ScriptableObject {
 
     public float RunSpeed = 10;
 
+    public float RollDuration;
+
+    public AnimationCurve RollCurve;
+
     public Vector2 jumpForce = new Vector2(0f, 1f);
 }
--- Assets/Runner/Script/Runner.cs
+++ Assets/Runner/Script/Runner.cs
@@ -9,21 +9,23 @@ public class Runner : MonoBehaviour {
     public static Signal Dead = new Signal();
 
     public GameParams gameParams;
-
+    
     Rigidbody2D body;
+
+    private float elapsedRollTime = 0f;
 
 	void Start () {
         body = GetComponent<Rigidbody2D>();
         InputManager.Swipe.AddListener(OnSwipe);
 	}
 
-    private void OnSwipe(InputManager.SwipeDirection direction) {
+    void OnSwipe(InputManager.SwipeDirection direction) {
         switch(direction) {
             case InputManager.SwipeDirection.Up:
                 Jump();
                 break;
             case InputManager.SwipeDirection.Down:
-                Roll();
+                StartCoroutine(Roll());
                 break;
             case InputManager.SwipeDirection.Left:
                 Block();
@@ -38,29 +40,49 @@ public class Runner : MonoBehaviour {
 
     }
 
-    private void Attack() {
+    void Update() {
+
 
     }
 
-    private void Block() {
-
-    }
-
-    private void Roll() {
-
-    }
-
-    private void Die() {
-        Dead.Dispatch();
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Obstacle")) Die();
     }
 
-    private void Jump() {
+    void Attack() {
+
+    }
+
+    void Block() {
+
+    }
+
+    IEnumerator Roll() {
+        float startYScale = transform.localScale.y;
+        float startGravityScale = body.gravityScale;
+        body.gravityScale = 50
+            ;
+
+        while (elapsedRollTime < gameParams.RollDuration)
+        {
+            elapsedRollTime += Time.deltaTime;
+            float yScaleFactor = gameParams.RollCurve.Evaluate(elapsedRollTime / gameParams.RollDuration);
+            transform.localScale = new Vector3(transform.localScale.x, startYScale * yScaleFactor, transform.localScale.z);
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        body.gravityScale = startGravityScale;
+        elapsedRollTime = 0;
+    }
+    
+    void Jump() {
         Debug.Log("jump!");
         body.AddForce(gameParams.jumpForce, ForceMode2D.Impulse);
+    }
+
+    void Die() {
+        Dead.Dispatch();
     }
 }
